aa892f61a2c8145b2e20e06145168d194c549b4f
Liv Chamblis <42556166+LivChambliss@users.noreply.github.com>
2020-12-11 21:46:39 -0600

    Add files via upload

---
+++ Travelogue/Travelogue/Travelogue/Controllers/AddTripViewController.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+class AddTripViewController: UIViewController {
+
+    @IBOutlet weak var tripTitleTextField: UITextField!
+    @IBOutlet weak var tripDescriptionTextBox: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func saveTrip(_ sender: Any) {
+        guard let tripTitle = tripTitleTextField.text, let tripDescription = tripDescriptionTextBox.text else {
+            print("No trip title or description set!")
+            return
+        }
+        
+        let trip = Trip(title: tripTitle, contents: tripDescription)
+        
+        do {
+            try trip?.managedObjectContext?.save()
+            
+            self.navigationController?.popViewController(animated: true)
+        } catch {
+            print("Could not save trip")
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
